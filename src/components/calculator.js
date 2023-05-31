2ac28543b26e3c6a732fb874fd05a0b56070fc0c
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-05-31 17:40:41 +0100

    Add logic folder and update calculator file

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,44 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../Logic/Calculate';
+import Button from './Button';
 import './calculator.css';
 
 function Calculator() {
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
+
+  function clickEvent(buttonName) {
+    const solution = calculate({ total, next, operation }, buttonName);
+    setTotal(solution.total);
+    setNext(solution.next);
+    setOperation(solution.operation);
+  }
+
   return (
     <div className="calc-container">
       <div className="output">
-        <div className="previous-operand">200 ÷</div>
-        <div className="current-operand">10</div>
+        <div className="previous-operand">{next ? total : operation || ''}</div>
+        <div className="current-operand">{next || total}</div>
       </div>
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="side-color">
-        ÷
-      </button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="side-color">
-        +
-      </button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="side-color">
-        *
-      </button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="side-color">
-        -
-      </button>
-      <button type="button" className="span-2">
-        0
-      </button>
-      <button type="button">.</button>
-      <button type="button" className="side-color">
-        =
-      </button>
+      <Button name="AC" className="" clickEvent={() => clickEvent('AC')} />
+      <Button name="+/-" className="" clickEvent={() => clickEvent('+/-')} />
+      <Button name="%" className="" clickEvent={() => clickEvent('%')} />
+      <Button
+        name="÷"
+        className="side-color"
+        clickEvent={() => clickEvent('÷')}
+      />
+      <Button name="1" className="" clickEvent={() => clickEvent('1')} />
+      <Button name="2" className="" clickEvent={() => clickEvent('2')} />
+      <Button name="3" className="" clickEvent={() => clickEvent('3')} />
+      <Button
+        name="+"
+        className="side-color"
+        clickEvent={() => clickEvent('+')}
+      />
+      <Button name="4" className="" clickEvent={() => clickEvent('4')} />
+      <Button name="5" className="" clickEvent={() => clickEvent('5')} />
+      <Button name="6" className="" clickEvent={() => clickEvent('6')} />
+      <Button
+        name="x"
+        className="side-color"
+        clickEvent={() => clickEvent('x')}
+      />
+      <Button name="7" className="" clickEvent={() => clickEvent('7')} />
+      <Button name="8" className="" clickEvent={() => clickEvent('8')} />
+      <Button name="9" className="" clickEvent={() => clickEvent('9')} />
+      <Button
+        name="-"
+        className="side-color"
+        clickEvent={() => clickEvent('-')}
+      />
+      <Button name="0" className="span-2" clickEvent={() => clickEvent('0')} />
+      <Button name="." className="" clickEvent={() => clickEvent('.')} />
+      <Button
+        name="="
+        className="side-color"
+        clickEvent={() => clickEvent('=')}
+      />
     </div>
   );
 }
